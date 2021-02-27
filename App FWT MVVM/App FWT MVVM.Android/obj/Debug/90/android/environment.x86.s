2cc30fb643387a700e45808ce25d28b56ac51b14
Aless1313 <alessandro.uanl@gmail.com>
2021-02-27 09:06:00 -0600

    Se añadieron 2 pantallas

--- App FWT MVVM/App FWT MVVM.Android/obj/Debug/90/android/environment.x86.s
+++ App FWT MVVM/App FWT MVVM.Android/obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"41e79667-dc82-4c4e-bc73-301316778d87"
+	.asciz	"17cd02b1-493a-4e5b-80ca-0753668a201d"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
