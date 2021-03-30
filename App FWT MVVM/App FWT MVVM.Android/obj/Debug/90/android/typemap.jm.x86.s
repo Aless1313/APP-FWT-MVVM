f9ba4564625b3e24254605af47a0ce453ff5e202
Alessandro Elizondo <alessandro.uanl@gmail.com>
2021-03-29 23:39:12 -0600

    a

---
+++ App FWT MVVM/App FWT MVVM.Android/obj/Debug/90/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 7f0e71cf820da4d4bb72ac8423a18e3a4ea7b85b */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1371
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 359203
+	.include	"typemap.jm.inc"
